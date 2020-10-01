8be33185c3b91f1c225e23f28500beb63135aa64
bartolomemejia <bart.1890mr@gmail.com>
2020-09-30 22:21:50 -0600

    change url for Pedro

--- public/src/scripts/constants/app.constants.js
+++ public/src/scripts/constants/app.constants.js
@@ -4,5 +4,7 @@
 
 	//.constant('API_URL', 'http://localhost:8080/creditos/public/ws/')
 	//.constant('API_URL', 'http://206.189.68.122/ws/') //Mario
-	.constant('API_URL', 'http://157.230.9.234/ws/') //Pedro
+	//.constant('API_URL', 'http://157.230.9.234/ws/') crediHelp
+	.constant('API_URL', 'http://157.245.174.7/ws/') //Pedro
+
 }())
