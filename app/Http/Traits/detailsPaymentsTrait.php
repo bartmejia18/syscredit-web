627c1fce023bd37e82832c78b019eae90df7263c
bartolomemejia <bart.1890mr@gmail.com>
2021-02-10 22:27:05 -0600

    change object credit

--- app/Http/Traits/detailsPaymentsTrait.php
+++ app/Http/Traits/detailsPaymentsTrait.php
@@ -6,33 +6,30 @@ use App\Creditos;
 use App\DetallePagos;
 
 trait detailsPaymentsTrait {
-    public function getDetailsPayments($creditId) {
+     public function getDetailsPayments($credit) {
         
         $detailPayment = new \stdClass();
-        $credit = Creditos::where("id", $creditId)->where("estado", 1)->with('planes', 'montos')->first();        
-        if ($credit) {
-            $detailsPayments = DetallePagos::where('credito_id', $creditId)->where('estado', 1)->get();
-            
-            if ($detailsPayments->count() > 0) {    
-                $detailPayment->allPayments = $detailsPayments; 
-                $detailPayment->totalPayment = $detailsPayments->sum('abono');
-                $detailPayment->totalFees =  intval($detailPayment->totalPayment/$credit->cuota_diaria);   
-                $detailPayment->pendingFees = $credit->planes->dias - $detailPayment->totalFees; 
-                $paymentPaid = $detailPayment->totalPayment % $credit->cuota_diaria;
-                if ($paymentPaid != 0) { 
-                    $detailPayment->paymentPaid = $paymentPaid;
-                } else {
-                    $detailPayment->paymentPaid = 0;
-                }
-                $detailPayment->paymentPercentage = round(($detailPayment->totalFees * 100)/($credit->planes->dias), 2);
+        $detailsPayments = DetallePagos::where('credito_id', $credit->id)->where('estado', 1)->get();
+        
+        if ($detailsPayments->count() > 0) {    
+            $detailPayment->allPayments = $detailsPayments; 
+            $detailPayment->totalPayment = $detailsPayments->sum('abono');
+            $detailPayment->totalFees =  intval($detailPayment->totalPayment/$credit->cuota_diaria);   
+            $detailPayment->pendingFees = $credit->planes->dias - $detailPayment->totalFees; 
+            $paymentPaid = $detailPayment->totalPayment % $credit->cuota_diaria;
+            if ($paymentPaid != 0) { 
+                $detailPayment->paymentPaid = $paymentPaid;
             } else {
-                $detailPayment->allPayments = [];
-                $detailPayment->totalPayment = 0;
-                $detailPayment->totalFees = 0;
-                $detailPayment->pendingFees = $credit->planes->dias;
                 $detailPayment->paymentPaid = 0;
-                $detailPayment->paymentPercentage = 0;
             }
+            $detailPayment->paymentPercentage = round(($detailPayment->totalFees * 100)/($credit->planes->dias), 2);
+        } else {
+            $detailPayment->allPayments = [];
+            $detailPayment->totalPayment = 0;
+            $detailPayment->totalFees = 0;
+            $detailPayment->pendingFees = $credit->planes->dias;
+            $detailPayment->paymentPaid = 0;
+            $detailPayment->paymentPercentage = 0;
         }
         return $detailPayment;
     }
@@ -65,18 +62,19 @@ trait detailsPaymentsTrait {
         return $detailPayment;
     }
 
-    public function getDetailsForCollector($creditId, $date) {
+    public function getDetailsForCollector($credit, $date) {
         
-        $detailPayment = new \stdClass();
-        $credit = Creditos::where("id", $creditId)->with('planes', 'montos')->first();        
-        if($credit && $credit->estado != 2){
-            $detailsPayments = DetallePagos::where('credito_id', $credit->id)->where('estado', 1)->where('fecha_pago', $date)->get();
-            
-            if($detailsPayments->count() > 0){     
+        $detailPayment = new \stdClass();       
+        if ($credit->estado != 2) {
+            $detailsPayments = DetallePagos::where('credito_id', $credit->id)
+                                            ->where('estado', 1)
+                                            ->where('fecha_pago', $date)
+                                            ->get();
+            if ($detailsPayments->count() > 0) {     
                 $detailPayment->totalPayment = $detailsPayments->sum('abono');
                 $detailPayment->totalFees =  intval($detailPayment->totalPayment/$credit->cuota_diaria);   
                 $paymentPaid = $detailPayment->totalPayment % $credit->cuota_diaria;
-                if($paymentPaid != 0){ 
+                if ($paymentPaid != 0) { 
                     $detailPayment->paymentPaid = $paymentPaid;
                 } else {
                     $detailPayment->paymentPaid = 0;
@@ -89,7 +87,6 @@ trait detailsPaymentsTrait {
                 $detailPayment->paymentPercentage = 0;
             }
         }
-        
         return $detailPayment;
     }
 
