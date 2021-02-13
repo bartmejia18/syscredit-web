04a0782eda92a31223f6fc0beff7c2b18bba43ac
bartolomemejia <bart.1890mr@gmail.com>
2021-02-12 21:52:30 -0600

    update name master

--- public/src/scripts/constants/app.constants.js
+++ public/src/scripts/constants/app.constants.js
@@ -4,7 +4,7 @@
 
 	//.constant('API_URL', 'http://localhost:8080/creditos/public/ws/')
 	.constant('API_URL', 'http://206.189.68.122/ws/') //Mario
-	//.constant('API_URL', 'http://157.230.9.234/ws/') crediHelp
+	//.constant('API_URL', 'http://157.230.9.234/ws/') RapiCredit
 	//.constant('API_URL', 'http://157.245.174.7/ws/') //Pedro
 
 }())
