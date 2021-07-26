2fe0fb281c3f38155effc73e332c3033bff1abef
Bartolomé Mejía <bart.1890mr@gmail.com>
2021-07-26 17:34:09 -0600

    fix url

--- public/src/scripts/constants/app.constants.js
+++ public/src/scripts/constants/app.constants.js
@@ -3,6 +3,6 @@
 	angular.module("app.constants", [])
 
 	//.constant('API_URL', 'http://localhost:8080/creditos/public/ws/')
-	.constant('API_URL', 'http://147.182.202.110//ws/')
+	.constant('API_URL', 'http://147.182.202.110/ws/')
 
 }())
