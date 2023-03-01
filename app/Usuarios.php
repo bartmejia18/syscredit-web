2cc75a745fe7beda8bddcedfb3797a070d40ef55
Bartolomé Mejía <bart.1890mr@gmail.com>
2023-03-01 12:19:09 -0600

    deleted_at field deleted

--- app/Usuarios.php
+++ app/Usuarios.php
@@ -4,14 +4,13 @@ namespace App;
 
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
 class Usuarios extends Model implements AuthenticatableContract, CanResetPasswordContract {
 	
-	use Authenticatable, CanResetPassword, SoftDeletes;
+	use Authenticatable, CanResetPassword;
 
 
 	protected $table = 'usuarios';
