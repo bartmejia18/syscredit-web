542531decdf5b0c992d184df3f585768fb813abf
Bartolomé Mejía <bart.1890mr@gmail.com>
2023-03-13 19:20:20 -0600

    change url new server

--- public/src/scripts/constants/app.constants.js
+++ public/src/scripts/constants/app.constants.js
@@ -6,6 +6,6 @@
 	//.constant('API_URL', 'http://144.126.216.191/ws/') //Mario
 	//.constant('API_URL', 'http://157.230.9.234/ws/') RapiCredit
 	//.constant('API_URL', 'http://157.245.174.7/ws/') //Pedro
-	.constant('API_URL', 'http://137.184.156.4/ws/') //ElPrimo
+	.constant('API_URL', 'http://207.154.229.193/ws/') //ElPrimo
 
 }())
