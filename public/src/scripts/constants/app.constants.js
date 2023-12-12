beb1eea5d3c2c18350c5b69538b9038b68cea409
Bartolomé Mejía <bart.1890mr@gmail.com>
2023-12-12 16:57:35 -0600

    change url

--- public/src/scripts/constants/app.constants.js
+++ public/src/scripts/constants/app.constants.js
@@ -5,6 +5,6 @@
 	//.constant('API_URL', 'http://localhost/syscredit-web/public/ws/')
 	//.constant('API_URL', 'http://144.126.216.191/ws/') //Mario
 	//.constant('API_URL', 'http://207.154.229.193/ws/') //ElPrimo
-	.constant('API_URL', 'http://64.23.132.184/ws/') //De Leon
+	.constant('API_URL', 'http://137.184.188.216/ws/') //De Leon
 
 }())
