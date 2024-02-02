2d8b7e348c8f8b14f4546917afb97d8a238e8572
Bartolomé Mejía <bart.1890mr@gmail.com>
2024-02-01 22:26:28 -0600

    fix initial credit

--- app/Http/Traits/detailsCreditsTrait.php
+++ app/Http/Traits/detailsCreditsTrait.php
@@ -99,33 +99,35 @@ trait detailsCreditsTrait {
         $dateInitial = $credit->fecha_inicio;
         $dateFinal = $this->getDateFinalCredit($credit);
     
-
-        $totalDays = (strtotime($dateInitial) - strtotime($dateFinal))/86400;
-        $totalDays = abs($totalDays); 
-        $totalDays = floor($totalDays + 1);	
-    
+        if ($dateInitial <= date('Y-m-d')) {
+            $totalDays = (strtotime($dateInitial) - strtotime($dateFinal))/86400;
+            $totalDays = abs($totalDays); 
+            $totalDays = floor($totalDays + 1);	
         
-        if ($credit->planes->tipo == 0 || $credit->planes->tipo == 1) {
-            $countSundayTemporal = 0;
-            if ($credit->planes->domingo == 1) {
-                for ($i=0; $i<$totalDays; $i++)  {  
-                    $dateTemporal = strtotime('+'.$i.'day', strtotime($dateInitial));
-                    $dateTemporal = date('d-m-Y', $dateTemporal);
-                    $dateTemporalNew = new \DateTime($dateTemporal);
-                    $sundayTemporal = date("D", $dateTemporalNew->getTimestamp());
+            if ($credit->planes->tipo == 0 || $credit->planes->tipo == 1) {
+                $countSundayTemporal = 0;
+                if ($credit->planes->domingo == 1) {
+                    for ($i=0; $i<$totalDays; $i++)  {  
+                        $dateTemporal = strtotime('+'.$i.'day', strtotime($dateInitial));
+                        $dateTemporal = date('d-m-Y', $dateTemporal);
+                        $dateTemporalNew = new \DateTime($dateTemporal);
+                        $sundayTemporal = date("D", $dateTemporalNew->getTimestamp());
 
-                    if ($sundayTemporal == "Sun") {
-                        ++$countSundayTemporal;
+                        if ($sundayTemporal == "Sun") {
+                            ++$countSundayTemporal;
+                        }
                     }
                 }
+                $totalDays = $totalDays - $countSundayTemporal;
+            } else if ($credit->planes->tipo == 2) {
+                $totalDays = floor($totalDays / 7);
+            } else if ($credit->planes->tipo == 3) {
+                $totalDays = floor($totalDays / 30);
             }
-            $totalDays = $totalDays - $countSundayTemporal;
-        } else if ($credit->planes->tipo == 2) {
-            $totalDays = floor($totalDays / 7);
-        } else if ($credit->planes->tipo == 3) {
-            $totalDays = floor($totalDays / 30);
+            return $totalDays - $feePaid;
+        } else {
+            return 0;
         }
-        return $totalDays - $feePaid;
     }
 
     public function getTotalDaysArrears($credit) {
