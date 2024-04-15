0bfdcc49b510ab7bde2c802037ac12d8ac9b6d72
Bartolomé Mejía <bart.1890mr@gmail.com>
2024-04-14 18:30:11 -0600

    add urls demo

--- public/src/scripts/constants/app.constants.js
+++ public/src/scripts/constants/app.constants.js
@@ -2,7 +2,7 @@
 	"use strict";	
 	angular.module("app.constants", [])
 
-	.constant('API_URL', 'http://localhost/syscredit-web/public/ws/')
+	//.constant('API_URL', 'http://localhost/syscredit-web/public/ws/')
 	//.constant('API_URL', 'http://144.126.216.191/ws/') //Mario
-	//.constant('API_URL', 'http://164.92.107.193/ws/') //Pruebas
+	.constant('API_URL', 'http://164.92.107.193/ws/') //Pruebas
 }())
